e8e61dc0311fb9b91ffa70280c7bcaaeb0368174
Hildaware <102989334+Hildaware@users.noreply.github.com>
2026-08-26 14:54:09 -0400

    Update vanahub-test-addon.lua

--- addon/vanahub-test-addon/vanahub-test-addon.lua
+++ addon/vanahub-test-addon/vanahub-test-addon.lua
@@ -1,6 +1,6 @@
 addon.name = 'vanahub-test-addon';
 addon.author = 'Hildaware';
-addon.version = '0.1.5';
+addon.version = '0.1.6';
 addon.desc = 'A minimal addon used to test VanaHub installation.';
 addon.link = 'https://github.com/Hildaware/vanahub-test-addon';
 
